d32612558c0f0e24bfdf0af7a0cde7b7cb39f5e5
shuiruge <shuiruge@whu.edu.cn>
2025-05-27 22:31:32 +0800

    Fix.

--- experiments/bifurcation.py
+++ experiments/bifurcation.py
@@ -20,24 +20,16 @@ def numpy_collate(batch):
   """
   return tree_map(np.asarray, default_collate(batch))
 
-def resize_flatten_cast(pic):
-  """Convert PIL image to flat (1-dimensional) numpy array."""
+def reshape_cast(pic):
+  """Output shape: [None, 28, 28, 1]."""
   return np.expand_dims(np.array(pic, dtype=jnp.float32), axis=-1)
 
 # Define our dataset, using torch datasets
-mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=resize_flatten_cast)
+mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=reshape_cast)
 # Create pytorch data loader with custom collate function
 training_generator = DataLoader(mnist_dataset, batch_size=batch_size, collate_fn=numpy_collate)
-
-# Get the full train dataset (for checking accuracy while training)
-train_images = np.array(mnist_dataset.train_data).reshape(len(mnist_dataset.train_data), -1)
-train_labels = np.array(mnist_dataset.train_labels)
-
-# Get full test dataset
-mnist_dataset_test = MNIST('/tmp/mnist/', download=True, train=False, transform=resize_flatten_cast)
-test_generator = DataLoader(mnist_dataset_test, batch_size=batch_size, collate_fn=numpy_collate)
-test_images = jnp.array(mnist_dataset_test.test_data.numpy().reshape(len(mnist_dataset_test.test_data), -1), dtype=jnp.float32)
-test_labels = np.array(mnist_dataset_test.test_labels)
+X_train = np.array(mnist_dataset.train_data)
+y_train = np.array(mnist_dataset.train_labels)
 
 
 # -------- Model --------- #
@@ -67,44 +59,53 @@ import optax
 class FFD(nnx.Module):
   def __init__(self, *, hidden_units):
     rngs = nnx.Rngs(random_key)
-    self.hidden = nnx.Linear(28*28, hidden_units, rngs=rngs)
-    self.output = nnx.Linear(hidden_units, 10, rngs=rngs)
+    self.hidden_layer = nnx.Linear(28*28, hidden_units, rngs=rngs)
+    self.output_layer = nnx.Linear(hidden_units, 10, rngs=rngs)
 
   def __call__(self, x):
     x = x.reshape(x.shape[0], -1)  # flatten
-    return self.output(nnx.relu(self.hidden(x)))
+    return self.output_layer(nnx.relu(self.hidden_layer(x)))
 
-def _add_noise(x, scale):
-  std_normal = random.truncated_normal(random.key(random_key), -1, 1, x.shape)
-  return x * (1 + scale * std_normal)
-
-def add_noise(model, noise_scale):
-  model.hidden.kernel.value = _add_noise(model.hidden.kernel.value, noise_scale)
-  model.output.kernel.value = _add_noise(model.output.kernel.value, noise_scale)
-
-def loss_fn(model, batch):
-  logits = model(batch['image'])
+def loss_fn(model, X, y):
+  logits = model(X)
   loss = optax.softmax_cross_entropy_with_integer_labels(
-    logits=logits, labels=batch['label']
+    logits=logits, labels=y
   ).mean()
   return loss
 
 @nnx.jit
-def train_step(model, optimizer: nnx.Optimizer, batch):
+def train_step(model, optimizer: nnx.Optimizer, X, y):
   """Train for a single step."""
   grad_fn = nnx.value_and_grad(loss_fn)
-  loss, grads = grad_fn(model, batch)
+  loss, grads = grad_fn(model, X, y)
   optimizer.update(grads)  # In-place updates.
   return loss
 
 def fit(model, learning_rate=1e-3, eval_every=200, epochs=3):
   optimizer = nnx.Optimizer(model, optax.adam(learning_rate))
   for epoch in range(epochs):
-    for step, (x, y) in enumerate(training_generator):
-      train_step(model, optimizer, {'image': x, 'label': y})
+    for step, (X, y) in enumerate(training_generator):
+      train_step(model, optimizer, X, y)
 
+def training_loss(model):
+  return loss_fn(model, X_train, y_train)
+
+def training_accuracy(model):
+  predicted_class = jnp.argmax(model(X_train), axis=1)
+  return jnp.mean(predicted_class == y_train)
+
+
+# -------- Bifurcation --------- #
 
 import matplotlib.pyplot as plt
+
+def _add_noise(x, scale):
+  std_normal = random.truncated_normal(random.key(random_key), -1, 1, x.shape)
+  return x * (1 + scale * std_normal)
+
+def add_noise(model, noise_scale):
+  model.hidden_layer.kernel.value = _add_noise(model.hidden_layer.kernel.value, noise_scale)
+  model.output_layer.kernel.value = _add_noise(model.output_layer.kernel.value, noise_scale)
 
 def relative_diff(x, y, precision):
   sum_ = jnp.abs(x) + jnp.abs(y)
@@ -113,7 +114,6 @@ def relative_diff(x, y, precision):
 
 def plot_difference(x, y, precision=1e-2):
   abs_diff = jnp.abs(x - y)
-  # Relative difference
   rel_diff = relative_diff(x, y, precision)
   # Plot
   fig, axs = plt.subplots(2, 1)
@@ -128,23 +128,16 @@ model2 = FFD(hidden_units=512)
 add_noise(model2, 1e-2)
 fit(model2)
 
-plot_difference(model.hidden.kernel.value, model2.hidden.kernel.value)
-plot_difference(model.hidden.bias.value, model2.hidden.bias.value)
-plot_difference(model.output.kernel.value, model2.output.kernel.value)
-plot_difference(model.output.bias.value, model2.output.bias.value)
+plot_difference(model.hidden_layer.kernel.value, model2.hidden_layer.kernel.value)
+plot_difference(model.hidden_layer.bias.value, model2.hidden_layer.bias.value)
+plot_difference(model.output_layer.kernel.value, model2.output_layer.kernel.value)
+plot_difference(model.output_layer.bias.value, model2.output_layer.bias.value)
 
 # model3 = nnx.clone(model2)
-# model3.output.kernel.value = model3.output.kernel.value * (1 +
-#   jnp.where(relative_diff(model.output.kernel.value, model2.output.kernel.value, 1e-2) > 0.5,
-#             0., 0.5 * random.normal(random.key(42), model.output.kernel.value.shape))
+# model3.output_layer.kernel.value = model3.output_layer.kernel.value * (1 +
+#   jnp.where(relative_diff(model.output_layer.kernel.value, model2.output_layer.kernel.value, 1e-2) > 0.5,
+#             0., 0.5 * random.normal(random.key(42), model.output_layer.kernel.value.shape))
 # )
-
-def training_loss(model):
-  return loss_fn(model, {'image': train_images, 'label': train_labels})
-
-def training_accuracy(model):
-  predicted_class = jnp.argmax(model(train_images), axis=1)
-  return jnp.mean(predicted_class == train_labels)
 
 print(training_loss(model))  # => 0.28564915
 print(training_loss(model2))  # => 0.44535416
